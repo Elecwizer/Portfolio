10b0ae3c0f93c561ff38f2d9b8a0a2909a12356f
elecwizer <elecwizer@gmail.com>
2025-03-12 11:32:30 +0300

    optimized navbar

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import HomePage from './components/HomePage';
 import Projects from './components/Projects';
 import Academics from './components/Academics';
 import About from './components/About';
+import NavBar from './components/NavBar';
 //end of imports
 
 function App() {
   return (
     <Router>
+      <NavBar/>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/projects" element={<Projects />} />
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,81 +1,9 @@
-import {useState} from "react";
-import { Link } from 'react-router-dom';
+
 //end of imports
 
 function HomePage(){
-    const [darkMode, setDarkMode] = useState(false);
-
-    const toggleDarkMode = () => {
-        const html = document.documentElement;
-        if(darkMode){
-            html.removeAttribute("data-bs-theme");
-        }else{
-            html.setAttribute("data-bs-theme", "dark");
-        }
-        setDarkMode(!darkMode);
-    };
-
     return (
         <>
-          <header className="text-center my-3">
-            <a href="https://www.kfupm.edu.sa/">
-              <img
-                src="https://specialneeds.kfupm.edu.sa/images/kfupm-logo-en.png"
-                alt="KFUPM logo"
-                className="img-fluid"
-              />
-            </a>
-          </header>
-
-          <nav className="navbar navbar-expand-lg navbar-light">
-            <div className="container" id="navbar">
-              <Link className="navbar-brand" to="/">
-                Portfolio
-              </Link>
-              <button
-                className="navbar-toggler"
-                type="button"
-                data-bs-toggle="collapse"
-                data-bs-target="#navbarNav"
-              >
-                <span className="navbar-toggler-icon"></span>
-              </button>
-              <div className="collapse navbar-collapse" id="navbarNav">
-                <ul className="navbar-nav ms-auto">
-                  <li className="nav-item">
-                    <Link className="nav-link" to="/">
-                      Home
-                    </Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link className="nav-link" to="/projects">
-                      Projects
-                    </Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link className="nav-link" to="/academics">
-                      Academics
-                    </Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link className="nav-link" to="/about">
-                      About
-                    </Link>
-                  </li>
-                </ul>
-                <button
-                  id="modeToggle"
-                  className={`btn ms-2 ${
-                    darkMode ? "btn-outline-light" : "btn-outline-dark"
-                  }`}
-                  onClick={toggleDarkMode}
-                >
-                  Toggle Theme
-                </button>
-              </div>
-            </div>
-          </nav>
-
           <main className="container text-center my-5">
             <h1 className="fw-bold">Hi, I'm Oukba Bouketir</h1>
             <h2 className="text-muted">
